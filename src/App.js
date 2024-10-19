d0a8f1858cc3f4bd16ca995b5ccabc416097242d
AmanSalman <rapamansalman@gmail.com>
2024-10-19 15:31:05 +0300

    fix cors

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ import cors from 'cors'
 export const Appinit = async (app, express) => {
   await connDB();
   app.use(express.json());
-  app.use(cors({
-    origin: 'http://127.0.0.1:5500', // specify the allowed origin
-  }));
+  app.use(cors());
   app.get("/", (req, res) => {
     return res.status(200).json({ message: "Welcome" });
   });
